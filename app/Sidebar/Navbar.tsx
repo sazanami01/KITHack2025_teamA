5e5412da8ebe229499a47f7620c1d4959323e072
10192006haruto <haruto10192006@gmail.com>
2025-09-12 14:54:57 +0900

    鈴木シラバス

---
+++ app/Sidebar/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+
+  return (
+
+    <nav className="w-full p-4 font-semibold">
+
+      Navbar
+
+    </nav>
+
+  );
+
+};
+
+
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import './globals.css'
+import { Layout } from "./styles/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>MyApp</h1>
+    </Layout>
+  )
+}
